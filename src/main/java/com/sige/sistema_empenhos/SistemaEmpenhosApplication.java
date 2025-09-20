498d70e2d82167aa7e286f748c3813a9063335d8
leticiabs <leticiabsilva03@gmail.com>
2025-09-20 20:08:35 -0300

    feat: first commit

---
+++ src/main/java/com/sige/sistema_empenhos/SistemaEmpenhosApplication.java
@@ -0,0 +1,13 @@
+package com.sige.sistema_empenhos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaEmpenhosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaEmpenhosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sige/sistema_empenhos/SistemaEmpenhosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sige.sistema_empenhos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaEmpenhosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
