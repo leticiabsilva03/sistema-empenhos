f82e9f542ef45d167287ad6a1879aa77a9c1debe
leticiabs <leticiabsilva03@gmail.com>
2025-09-27 14:19:58 -0300

    feat: creating repositories

--- src/main/java/com/sige/sistema_empenhos/repositories/EmpenhoRepository.java
+++ src/main/java/com/sige/sistema_empenhos/repositories/EmpenhoRepository.java
@@ -3,7 +3,5 @@ package com.sige.sistema_empenhos.repositories;
 import com.sige.sistema_empenhos.entities.empenho.EmpenhoEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.UUID;
-
 public interface EmpenhoRepository extends JpaRepository<EmpenhoEntity, Long> {
 }
--- src/main/java/com/sige/sistema_empenhos/repositories/PagamentoRepository.java
+++ src/main/java/com/sige/sistema_empenhos/repositories/PagamentoRepository.java
@@ -3,7 +3,5 @@ package com.sige.sistema_empenhos.repositories;
 import com.sige.sistema_empenhos.entities.pagamento.PagamentoEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.UUID;
-
 public interface PagamentoRepository extends JpaRepository<PagamentoEntity, Long> {
 }
