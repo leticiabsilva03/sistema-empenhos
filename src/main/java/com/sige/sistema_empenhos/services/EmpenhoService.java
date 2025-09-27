5c18f568507074dc9d463b4c7bb6be2212a04127
leticiabs <leticiabsilva03@gmail.com>
2025-09-27 14:22:07 -0300

    feat: creating empenho service

---
+++ src/main/java/com/sige/sistema_empenhos/services/EmpenhoService.java
@@ -0,0 +1,56 @@
+package com.sige.sistema_empenhos.services;
+
+import com.sige.sistema_empenhos.dtos.EmpenhoRecordDto;
+import com.sige.sistema_empenhos.entities.empenho.EmpenhoEntity;
+import com.sige.sistema_empenhos.repositories.EmpenhoRepository;
+import com.sige.sistema_empenhos.exceptions.EmpenhoNotFoundException;
+import com.sige.sistema_empenhos.exceptions.EmpenhoStatusException;
+import jakarta.transaction.Transactional;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class EmpenhoService {
+
+    private final EmpenhoRepository empenhoRepository;
+
+    public EmpenhoService(EmpenhoRepository empenhoRepository){
+        this.empenhoRepository = empenhoRepository;
+    }
+
+    @Transactional
+    public EmpenhoEntity save(EmpenhoRecordDto empenhoRecordDto){
+        EmpenhoEntity empenhoEntity = new EmpenhoEntity(
+                empenhoRecordDto.descricaoEmpenho(),
+                empenhoRecordDto.valorEmpenho(),
+                empenhoRecordDto.dataEmpenho()
+        );
+        empenhoEntity.setStatusEmpenho(EmpenhoEntity.Status.ABERTO);
+        return empenhoRepository.save(empenhoEntity);
+    }
+
+    public List<EmpenhoEntity> findAll(){
+        return empenhoRepository.findAll();
+    }
+
+    public EmpenhoEntity findById(Long id){
+        return empenhoRepository.findById(id)
+                .orElseThrow(()->new EmpenhoNotFoundException("Empenho não encontrado!"));
+    }
+
+    @Transactional
+    public void delete(Long id){
+        EmpenhoEntity empenho = empenhoRepository.findById(id)
+                .orElseThrow(()->new EmpenhoNotFoundException("Empenho não encontrado!"));
+        if (empenho.getStatusEmpenho() == EmpenhoEntity.Status.LIQUIDADO){
+            throw new EmpenhoStatusException("Não é possível excluir empenho liquidado. Estorne a liquidação antes.");
+        }
+        if (empenho.getStatusEmpenho() == EmpenhoEntity.Status.PAGO){
+            throw new EmpenhoStatusException("Não é possível excluir empenho pago. Estorne o pagamento e a liquidação antes.");
+        }
+
+        empenhoRepository.delete(empenho);
+    }
+
+}
